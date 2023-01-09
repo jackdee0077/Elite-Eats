21cdc3de2e49d482e95b0aacd3677042e1e9e791
daniel jackson <jackdee0077@gmail.com>
2023-01-09 14:33:41 -0500

    resolving conflicts

--- Elite_eats_site/Elite_eats_app/urls.py
+++ Elite_eats_site/Elite_eats_app/urls.py
@@ -1,18 +1,12 @@
 from django.urls import path
 
 
-from. views import HomePageView, SearchResultsView
+from. views import HomePageView, SearchResultsView, DirectoryView, PostView
 
 urlpatterns = [
     path("search/", SearchResultsView.as_view(), name="search_results"),
     path("", HomePageView.as_view(), name="home"),
-]
-
-from Elite_eats_app.views import DirectoryView, PostView
-
-
-urlpatterns = [
     path('directory.html', DirectoryView.as_view(), name='directory'),
     path('review.html', PostView.as_view(), name='review'),
-    ]
+]
 
