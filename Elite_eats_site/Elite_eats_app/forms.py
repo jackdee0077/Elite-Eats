e79be0d19c65e492306dd899e2cca39bf40bfc4e
Jeriel Slay <SlayJ@protonmail.com>
2023-01-11 21:16:58 -0500

    newer updated added

--- Elite_eats_site/Elite_eats_app/forms.py
+++ Elite_eats_site/Elite_eats_app/forms.py
@@ -1,7 +1,13 @@
 from django.forms import ModelForm
-from Elite_eats_app.models import Post
+from .models import Post, Image, Restaurant
 
 class PostForm(ModelForm):
     class Meta:
         model = Post
-        fields = ['review']
+        restaurant = Restaurant.name
+        fields = ['review', 'restaurant']
+
+class ImageForm(ModelForm):
+    class Meta:
+        model = Image
+        fields = ('title', 'image',)
