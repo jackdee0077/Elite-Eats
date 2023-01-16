d05a1d7ce777805e1a919022e11a49f9e2da2904
Jeriel Slay <SlayJ@protonmail.com>
2023-01-15 21:04:13 -0500

    new updates

--- Elite_eats_site/Elite_eats_app/urls.py
+++ Elite_eats_site/Elite_eats_app/urls.py
@@ -1,14 +1,15 @@
 from django.urls import path
 
-from Elite_eats_app.views import DirectoryView, PostView, HomePageView, SearchResultsView, TrendingView, image_upload_view, update_review
+from Elite_eats_app.views import DirectoryView, PostView, HomePageView, SearchResultsView, TrendingView, ImageUploadView, update_review, delete_review
 
 urlpatterns = [
-    path("", HomePageView.as_view(), name="home"),
+    path("", HomePageView.as_view(), name='home'),
     path('directory.html', DirectoryView.as_view(), name='directory'),
     path('review.html', PostView.as_view(), name='review'),
     path("search/", SearchResultsView.as_view(), name='search_results'),
     path('trending.html', TrendingView.as_view(), name='trending'),
-    path('upload', image_upload_view, name='upload'),
-    path('update_review/<review_id>', update_review, name='update-review'),
+    path('upload', ImageUploadView.as_view(), name='upload'),
+    path('update_review/<int:review_id>', update_review, name='update-review'),
+    path('delete_review/<int:review_id>', delete_review, name='delete-review'),
     ]
 
